cdc8381de002df8f1539973402e07d2c9db3d193
kethllen <kety_andrey@hotmail.com>
2022-01-22 10:48:40 -0300

    backup user

--- back-end/index.js
+++ back-end/index.js
@@ -7,6 +7,10 @@ server.use(json());
 
 const users = [];
 const tweets =[];
+let userBackup = {
+  username:"",
+  avatar:""
+}
 
 server.get('/users', (req, res) => {
     res.send(users);
@@ -15,13 +19,25 @@ server.get('/users', (req, res) => {
 server.post('/sign-up', (req, res) => {
     const user = req.body; 
     users.push(user);
+    userBackup.username = user.username;
+    userBackup.avatar = user.avatar;
     res.send("ok");
   });
 
 server.post('/tweets', (req, res) => {
   const tweet = req.body; 
-  tweets.push(tweet);
+  tweets.push({...userBackup, ...tweet});
   res.send("ok");
+});
+
+server.get('/tweets', (req, res) => {
+  if(tweets.length<=10){
+    res.send(tweets);
+  }else {
+    const tweetsReturn = tweets.slice(tweets.length-11, tweets.length);
+    res.send(tweetsReturn);
+  }
+
 });
 
 server.listen(5000,() => console.log("Init server"))
